e8220b4c878d0c8b8e62e1dce3c5543fe7c30549
Krishnag09 <krishnag0902@gmail.com>
2021-06-24 20:04:09 -0400

    Added automated steps to compose

--- selenium/socialplayselenium.py
+++ selenium/socialplayselenium.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -15,3 +16,16 @@ driver.find_element_by_css_selector("#button-container > div").click()
 time.sleep(2)
 driver.find_element_by_name("password").send_keys("dh_hacks2021")
 driver.find_element_by_css_selector("#button-container").click()
+time.sleep(2)
+driver.find_element_by_css_selector("#crisp-chatbox > div > a > span.cc-1c9v > span > span > span > span.cc-tkyh > span > span.cc-g0ak.cc-hy0f").click()
+create_button = driver.find_element_by_css_selector("#sidebar > div.menu.round-button.create-button")
+time.sleep(2)
+driver.execute_script("arguments[0].click();", create_button)
+time.sleep(2)
+musictype = driver.find_element_by_xpath("//*[contains(text(), 'Modern Cinematic')]") #enter string for the genre
+time.sleep(2)
+driver.execute_script("arguments[0].click();", musictype)
+time.sleep(2)
+driver.find_element_by_xpath("//*[contains(text(), 'Key Signature')]").click()
+time.sleep(3)
+driver.close()
