88dce4784a049993bc19567a72b4a81497e20b8b
bhavant <bhavan.channira@daily-harvest.com>
2021-06-24 22:03:11 -0400

    Code for Web Socket and Submit emotion to backend

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,38 @@
 const video = document.getElementById('video');
 let captureBtn;
 let videoElement;
+let socket;
+
+window.onload = function () {
+  socket = new WebSocket("ws://127.0.0.1:5000/communicate");
+
+  socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+
+    socket.send(JSON.stringify({ name: 'Thimmu' }));
+  };
+
+  socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function (error) {
+    console.log(`[error] ${error.message}`);
+  };
+}
+
+
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('../../static/js/models'),
@@ -24,26 +56,20 @@ function startVideo() {
 
 function captureEmotion() {
   captureBtn.style.display = 'none';
-  document.querySelector('#videoElement').style.display = 'block';
+  videoElement = document.querySelector('#videoElement');
+  videoElement.style.display = 'block';
   startVideo();
 }
 let displaySize;
-let canvas;
+let canvas = null;
 video.addEventListener('play', () => {
-  canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
-  displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvas, displaySize);
-  findEmotion();
-  // setInterval(async () => {
-  //   const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-  //   // console.log(detections[0].expressions);
-  //   const resizedDetections = faceapi.resizeResults(detections, displaySize)
-  //   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-  //   faceapi.draw.drawDetections(canvas, resizedDetections)
-  //   faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-  //   // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  // }, 100)
+  if (!canvas) {
+    canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+    findEmotion();
+  }
 });
 
 let emoInterval;
@@ -60,12 +86,12 @@ function findEmotion() {
   }, 100);
 }
 let emotionDisplayed = false;
+let disp_emotion = '';
 function dispEmotion(emotions) {
   // console.log(emotions);
   if (emotions && !emotionDisplayed) {
     emotionDisplayed = true;
     let max_confidence = 0;
-    let disp_emotion = '';
     let keys = Object.keys(emotions);
     for (let emotion of keys) {
       if (emotions[emotion] > max_confidence) {
@@ -81,6 +107,23 @@ function dispEmotion(emotions) {
 function retry() {
   document.querySelector('#emoResp').innerText = '';
   emotionDisplayed = false;
+  disp_emotion = '';
   clearInterval(emoInterval);
   findEmotion();
+}
+
+function sendToBackend() {
+  let toSend = { emotion: disp_emotion };
+  socket.send(JSON.stringify(toSend));
+  clearInterval(emoInterval);
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+  // var elem = document.querySelector('canvas');
+  // elem.parentNode.removeChild(elem);
+  canvas.remove();
+  canvas = null;
+  captureBtn.style.display = 'block';
+  videoElement.style.display = 'none';
+  document.querySelector('#emoResp').innerText = '';
+  emotionDisplayed = false;
+  disp_emotion = '';
 }
