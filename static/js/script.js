012b0ba652e9cfce20de9a5fabc3efd69025bad9
Victoria <victoriaho91@github.com>
2021-06-24 23:21:41 -0400

    grabs elements

--- static/js/script.js
+++ static/js/script.js
@@ -84,3 +84,17 @@ function retry() {
   clearInterval(emoInterval);
   findEmotion();
 }
+
+
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
